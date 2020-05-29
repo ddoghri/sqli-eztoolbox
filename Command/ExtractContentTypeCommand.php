a54b3c8ec66cefbbfbd8b196adf1c1fd2344a16d
daverner <daverner@sqli.com>
2020-05-29 19:51:05 +0200

    Correction of missing service
    Signed-off-by: daverner <daverner@sqli.com>

--- Command/ExtractContentTypeCommand.php
+++ Command/ExtractContentTypeCommand.php
@@ -2,20 +2,16 @@
 
 namespace SQLI\EzToolboxBundle\Command;
 
+use Monolog\Handler\StreamHandler;
+use Monolog\Logger;
+use SQLI\EzToolboxBundle\Services\ExtractHelper;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\DependencyInjection\ContainerInterface;
-use Monolog\Logger;
-use Monolog\Handler\StreamHandler;
-use SQLI\EzToolboxBundle\Helper\ExtractHelper;
-
-
 
 class ExtractContentTypeCommand extends ContainerAwareCommand
 {
-
     protected $log;
 
     /**
@@ -47,27 +43,25 @@ class ExtractContentTypeCommand extends ContainerAwareCommand
      */
     protected function execute( InputInterface $input, OutputInterface $output )
     {
-        $today = date("Y-m-d");
-        $this->log = new Logger('ExtractContentTypesCommandLogger');
-        $this->log->pushHandler(new StreamHandler($this->getContainer()->get('kernel')->getLogDir().'/extract_content_types-'.$today.'.log'));
+        $today     = date( "Y-m-d" );
+        $this->log = new Logger( 'ExtractContentTypesCommandLogger' );
+        $this->log->pushHandler( new StreamHandler( $this->getContainer()->get( 'kernel' )->getLogDir() . '/extract_content_types-' . $today . '.log' ) );
 
-        $this->log->addInfo("Debut de l'extract des content types");
+        $this->log->addInfo( "Debut de l'extract des content types" );
 
-        $outputFilename = $input->getArgument( 'filename');
-        $identifierContentType = $input->getArgument( 'identifierContentType');
-        $content = $this->getContainer()
-            ->get(ExtractHelper::class )
-            ->createContentToExport($identifierContentType, $this->log);
+        $outputFilename        = $input->getArgument( 'filename' );
+        $identifierContentType = $input->getArgument( 'identifierContentType' );
+        $content               = $this->getContainer()
+            ->get( ExtractHelper::class )
+            ->createContentToExport( $identifierContentType, $this->log );
 
         //Ecriture du content type dans un fichier
         //TODO : Voir pour paramaettrer le nom et chemin du fichier de sortie
         //ouverture du fichier en mode écriture, création du fichier s'il n'existe pas.
-        $fp=fopen($outputFilename, "w");
-        fwrite($fp,$content);
-        fclose($fp);
+        $fp = fopen( $outputFilename, "w" );
+        fwrite( $fp, $content );
+        fclose( $fp );
 
-        $this->log->addInfo("Fin de l'extract des content types");
-
+        $this->log->addInfo( "Fin de l'extract des content types" );
     }
-
 }
