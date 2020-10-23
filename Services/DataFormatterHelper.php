20665b99aa467cba2278081a9c91abdaa6198136
daverner <daverner@sqli.com>
2020-10-23 12:20:08 +0200

    Adding an slug format in DataFormatterHelper
    Signed-off-by: daverner <daverner@sqli.com>

--- Services/DataFormatterHelper.php
+++ Services/DataFormatterHelper.php
@@ -53,9 +53,30 @@ class DataFormatterHelper
                     $url = "http://". $data;
                 }
                 return $url;
+            case "slug":
+                return $this->slugify( $data );
         }
 
         throw new DataFormatterException( "Unknown format name : $format" );
+    }
+
+    /**
+     * @param string $string
+     * @param string $delimiter
+     * @return string
+     */
+    public function slugify( $string, $delimiter = '-' )
+    {
+        $oldLocale = setlocale( LC_ALL, '0' );
+        setlocale( LC_ALL, 'en_US.UTF-8' );
+        $clean = iconv( 'UTF-8', 'ASCII//TRANSLIT', $string );
+        $clean = preg_replace( "/[^a-zA-Z0-9\/_|+ -]/", '', $clean );
+        $clean = strtolower( $clean );
+        $clean = preg_replace( "/[\/_|+ -]+/", $delimiter, $clean );
+        $clean = trim( $clean, $delimiter );
+        setlocale( LC_ALL, $oldLocale );
+
+        return $clean;
     }
 
     /**
